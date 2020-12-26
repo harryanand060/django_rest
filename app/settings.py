5dfaa47bf5916336e38c29b724ff0ec77d01ceb5
anand kumar <harryanand060@gmail.com>
2020-12-26 16:48:04 +0530

    Test Celery for background task

--- app/settings.py
+++ app/settings.py
@@ -134,3 +134,11 @@ try:
     from app.auth_settings import *
 except ImportError as ex:
     raise Exception(f"Authentication Settings Not Imported {ex}")
+
+
+# celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
